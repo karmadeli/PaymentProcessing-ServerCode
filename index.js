add5d4017a151b440db06cc2daceb43fdec2b8ad
karmadeli <chadkarmadeli@gmail.com>
2018-06-28 10:24:06 -0400

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
+var liveKey = 'YOUR_LIVEKEY'
+var testKey = 'YOUR_TESTKEY'
+
 const express = require('express')
+var stripe = require('stripe')(testKey)
+var bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended : true
+}))
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.post('/charge', (req, res)=>{
+  var amount = req.body.amount
+  var currency = req.body.currency
+  var token = req.body.source
+  var email = req.body.email
+  var description = req.body.description
+ 
+  stripe.charges.create({
+    source : token,
+    currency : currency,
+    amount : amount,
+    receipt_email: email,
+    description: description
+    
+  }, function(err, charge){
+    if (err){
+       res.status(500).end()
+     }else{
+      res.status(200).send()
+     }
+  })
+
+})
+
+  app.listen(app.get('port'), function(){
+    console.log('Node app is running on port', app.get('port'))
+  } )
