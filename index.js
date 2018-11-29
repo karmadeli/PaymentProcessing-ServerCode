c1871f64d711eb3b394e88ffac86b57bb13c831f
karmadeli <chadkarmadeli@gmail.com>
2018-11-29 10:25:33 -0500

    tweaks

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 var stripe = require('stripe')(testKey)
 var bodyParser = require('body-parser')
 const path = require('path')
+
 const PORT = process.env.PORT || 5000
 var app = express()
 
